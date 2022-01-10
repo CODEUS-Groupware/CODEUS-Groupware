34de758c8059e38030a4acc49f3d4f195639f106
loadingKKamo21 <dornqhdlrnddp200@gmail.com>
2022-01-10 15:41:37 +0900

    회의실 예약 게시판 커밋 - 목록 게시판 통합(전체 목록 / 내 목록) - 일부 경로, 명칭 수정

--- CODEUS/src/main/java/com/codeusgroup/codeus/meetingResrv/model/dao/MeetingResrvDAO.java
+++ CODEUS/src/main/java/com/codeusgroup/codeus/meetingResrv/model/dao/MeetingResrvDAO.java
@@ -1,8 +1,39 @@
 package com.codeusgroup.codeus.meetingResrv.model.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.codeusgroup.codeus.meetingResrv.model.vo.MeetingResrv;
+import com.codeusgroup.codeus.meetingResrv.model.vo.PageInfo;
 
 @Repository("mrDAO")
 public class MeetingResrvDAO {
+    
+    public int getListCount(SqlSessionTemplate sqlSession) {
+        return sqlSession.selectOne("meetResrvMapper.getListCount");
+    }
+    
+    public ArrayList<MeetingResrv> selectList(SqlSessionTemplate sqlSession, PageInfo pi) {
+        int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+        RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+        
+        return (ArrayList) sqlSession.selectList("meetResrvMapper.selectList", pi, rowBounds);
+    }
+    
+    public int getMyListCount(SqlSessionTemplate sqlSession, String mId) {
+        return sqlSession.selectOne("meetResrvMapper.getMyListCount", mId);
+    }
+    
+    public ArrayList<MeetingResrv> selectMyList(SqlSessionTemplate sqlSession, HashMap map) {
+        
+        int offset = (((PageInfo) map.get("pi2")).getCurrentPage() - 1) * ((PageInfo) map.get("pi2")).getBoardLimit();
+        RowBounds rowBounds = new RowBounds(offset, ((PageInfo) map.get("pi2")).getBoardLimit());
+        
+        return (ArrayList) sqlSession.selectList("meetResrvMapper.selectMyList", map, rowBounds);
+    }
     
 }
